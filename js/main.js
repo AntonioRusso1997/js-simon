0b84c89e2342be02f11a0f8f81b6e2c81a8d81a2
Antonio <antonioru1997@gmail.com>
2021-07-29 16:45:39 +0200

    FINE ESERCIZIO

--- js/main.js
+++ js/main.js
@@ -4,25 +4,61 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 
-//Creo array con 5 numeri casuali
+//Creo array con 5 numeri casuali e un array per le risposte dell'utente
 let fiveNumbers = [];
+let userNumbers = [];
+let correctNumbers = [];
 
-//Ciclo for per pushare i 5 numeri casuali nell'array creato in precedenza.
-for (let i = 0; i < 5; i++) {
-    fiveNumbers.push(rndNumber(1,100) + " ");
-}
+//"Stampo" i 5 numeri da ricordare in un alert.
+rndArray();
+console.log("Numeri Simon: " + fiveNumbers)
 
-// "Stampo" i 5 numeri da ricordare.
-alert("RICORDA QUESTI NUMERI: "+ fiveNumbers);
-
-
-
-
-
+// Parte timer di 30 (5 secondi perchè Alessio è pigro) secondi.
+var timeleft = 5;
+var timer = setInterval(function(){
+  
+    if(timeleft == 0){
+    clearInterval(timer);
+    insertNumber();
+  } else {
+    console.log("Mancano " + timeleft + " secondi.");
+  }
+  timeleft--;
+}, 1000);
 
 /* -- FUNZIONI --*/
 
 //Funzione per generare numeri casuali
 function rndNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//Funzione per inserire i 5 numeri in un array + alert
+function rndArray () {
+    for (let i = 0; i < 5; i++) {
+        fiveNumbers.push(rndNumber(1,100));
+    }
+    alert("RICORDA QUESTI NUMERI: "+ fiveNumbers);
+}
+
+//Funzione per chiedere i 5 numeri in successione
+function insertNumber() {
+    for (let i= 0; i < 5; i++) {
+        let userNumber = parseInt(prompt("Inserisci numero."));
+        userNumbers.push(userNumber);
+        if (isInArray(fiveNumbers, userNumber) == true) {
+            correctNumbers.push(userNumber);
+        }      
+    }
+    console.log("Numeri inseriti: " + userNumbers);
+    console.log("Numeri indovinati: " + correctNumbers.length + " --> (" + correctNumbers + ")");
+}
+
+//Funzione per controllare se i numeri inseriti sono in un array
+
+function isInArray(array, element) {
+    if (array.includes(element))
+        return true;
+    
+    return false;
 }
